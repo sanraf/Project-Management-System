9532e63e62fc812a437806210bfdaf18b67cb80d
mbali92 <83025186+mbali92@users.noreply.github.com>
2024-04-09 14:53:55 +0200

    finished creating the logged in page

--- project-management-interface/src/Component/Login.tsx
+++ project-management-interface/src/Component/Login.tsx
@@ -8,11 +8,30 @@ function Login() {
           <div className="login_page">
               <div className="page-row">
                   <div className="user_login">
-                      
+                      <div className="user_login_details">
+                           <h5>Please register</h5>
+                            <p>New to our system? please register your details to use the system</p>
+                            <form action="">
+                                <h6>Full name</h6>
+                              <input id='fullname' type="text" placeholder='fullname' />
+                              <h6>Email</h6>
+                                <input type="text" id='email' placeholder='fullname' />
+                               <h6>Password</h6>
+                              <input id='password' type="text" placeholder='password' />
+                              <h6>Re-enter password</h6>
+                                <input id='password' type="text" placeholder='password' />
+                                <button type='submit'>Register</button>
+                            </form>
+                            <div className="third_party_login">
+                                <p id=''>Login with other sites</p>
+                                 <i className="lni lni-github-original"></i>
+                                <i className="lni lni-google"></i>
+                            </div>
+                      </div>
                   </div>
                   <div className="login_project_description">
                       <div className="login_project_title">
-                          <h1>We present to you name of project</h1>
+                          <h1>Hello Welcome, to Project Name</h1>
                           <p>Hello and welcome to ProjectMaster! We're delighted to have you join
                our community of project managers and teams who are achieving remarkable success with our powerful project
                 management platform.
