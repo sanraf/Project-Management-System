2fd897b36a7f6b689a49d807e195da0bf3341e3a
LCBA21 <120941567+LCBA21@users.noreply.github.com>
2024-04-18 09:44:35 +0200

    update:Updated authenticate endpoint to generate jwtToken and token id,created refresh Token endpoint

--- backend/src/main/java/com/algoExpert/demo/Controller/AuthController.java
+++ backend/src/main/java/com/algoExpert/demo/Controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.algoExpert.demo.Controller;
 
 import com.algoExpert.demo.Dto.AuthRequest;
+import com.algoExpert.demo.Dto.RefreshTokenRequest;
 import com.algoExpert.demo.Entity.Project;
+import com.algoExpert.demo.Entity.RefreshToken;
 import com.algoExpert.demo.Entity.User;
+import com.algoExpert.demo.Jwt.JwtResponse;
+import com.algoExpert.demo.Repository.RefreshTokenRepository;
 import com.algoExpert.demo.Repository.Service.AuthService;
 import com.algoExpert.demo.Jwt.JwtService;
+import com.algoExpert.demo.Repository.Service.Impl.RefreshTokenSevice;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,6 +23,7 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/auth")
+@Slf4j
 public class AuthController {
     @Autowired
     private AuthService authService;
@@ -26,6 +33,12 @@ public class AuthController {
 
     @Autowired
     private JwtService jwtService;
+
+    @Autowired
+    private RefreshTokenSevice tokenSevice;
+
+    @Autowired
+    private RefreshTokenRepository tokenRepository;
 //    create user
     @PostMapping("/registerUser")
     public User registerUser(@RequestBody User user){
@@ -50,13 +63,34 @@ public class AuthController {
     }
 
     @PostMapping("/authenticate")
-    public String authenticateAndGetToken(@ RequestBody AuthRequest authRequest){
+    public JwtResponse authenticateAndGetToken(@ RequestBody AuthRequest authRequest){
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
         if (authentication.isAuthenticated()) {
-            return jwtService.generateToken(authRequest.getUsername());
+            RefreshToken refreshToken = tokenSevice.createRefreshToken(authRequest.getUsername());
+        return   JwtResponse.builder()
+                     .jwtToken(jwtService.generateToken(authRequest.getUsername()))
+                     .refreshToken(refreshToken.getToken()).build();
         } else {
             throw new UsernameNotFoundException("invalid user request !");
         }
     }
+
+    @PostMapping("/refreshToken")
+    public JwtResponse refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest){
+
+    return  tokenSevice.findByToken(refreshTokenRequest.getToken())
+                .map(RefreshToken::getUser)
+                .map(user -> {
+                    String accessToken = jwtService.generateToken(user.getUsername());
+                    return  JwtResponse.builder()
+                            .jwtToken(accessToken)
+                            .refreshToken(refreshTokenRequest.getToken()).build();
+                }).orElseThrow(() ->new RuntimeException(
+                        "Refresh token is not in database"
+            ));
+
+    }
+
+
 
 }
