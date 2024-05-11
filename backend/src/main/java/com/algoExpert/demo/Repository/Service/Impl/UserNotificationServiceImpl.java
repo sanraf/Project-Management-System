7bd0357ddbcd6b3ef3746602a64def93c9ae0733
sanraf <santoschrist123@gmail.com>
2024-05-12 00:18:09 +0200

    ADD : simplified UserNotificationServiceImpl for creating notification and created deleteNotification method

--- backend/src/main/java/com/algoExpert/demo/Repository/Service/Impl/UserNotificationServiceImpl.java
+++ backend/src/main/java/com/algoExpert/demo/Repository/Service/Impl/UserNotificationServiceImpl.java
@@ -2,6 +2,8 @@ package com.algoExpert.demo.Repository.Service.Impl;
 
 import com.algoExpert.demo.Entity.User;
 import com.algoExpert.demo.Entity.UserNotification;
+import com.algoExpert.demo.ExceptionHandler.InvalidArgument;
+import com.algoExpert.demo.Repository.Service.ProjectUserService;
 import com.algoExpert.demo.Repository.Service.UserNotificationService;
 import com.algoExpert.demo.Repository.UserNotificationRepository;
 import com.algoExpert.demo.Repository.UserRepository;
@@ -11,35 +13,48 @@ import org.springframework.stereotype.Service;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
+import static com.algoExpert.demo.AppUtils.AppConstants.USER_NOT_FOUND;
+
 @Service
 public class UserNotificationServiceImpl implements UserNotificationService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private ProjectUserImpl ProjectUserImpl;
+    private ProjectUserService projectUserService;
     @Autowired
     private UserNotificationRepository notificationRepository;
     @Override
-    public void createNotification(User user, String notifMsg,int taskId) {
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MMM/yyyy HH:mm:ss");
-//
-//        UserNotification userNotification = UserNotification.builder()
-//                .notifMsg(notifMsg)
-//                .notifTime(simpleDateFormat.format(new Date()))
-//                .fullName(user.getFullName())
-//                .taskId(taskId)
-//                .user(user).build();
-//        notificationRepository.save(userNotification);
+    public void createNotification(User user, String notifMsg) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MMM/yyyy HH:mm:ss");
+
+
+        UserNotification userNotificationCreated = UserNotification.builder()
+                .notifMsg(notifMsg)
+                .notifTime(simpleDateFormat.format(new Date()))
+                .fullName(user.getFullName()).build();
+
+        List<UserNotification> userNotificationList =  user.getUserNotificationList();
+        userNotificationList.add(userNotificationCreated);
+        user.setUserNotificationList(userNotificationList);
+
+        userRepository.save(user);
     }
-//
+
 
     public List<UserNotification> findByLoginUser() {
-        return notificationRepository.getNotificationByUserId(ProjectUserImpl.loggedInUserId());
+        Integer userId = projectUserService.loggedInUserId();
+        return notificationRepository.getNotificationByUserId(userId);
     }
 
     @Override
     public void deleteNotification(int id) {
+        Integer userId = projectUserService.loggedInUserId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new InvalidArgument(String.format(USER_NOT_FOUND,userId)));
         UserNotification notification = notificationRepository.findById(id).orElseThrow();
+        List<UserNotification> userNotificationList = user.getUserNotificationList();
+        userNotificationList.remove(notification);
         notificationRepository.delete(notification);
     }
 
