db61687fd2a5882e021612cded81f3cbe0dc7b80
sanraf <santoschrist123@gmail.com>
2024-05-12 00:19:33 +0200

    ADD : added @Transactional in service for deletion of notification

--- backend/src/main/java/com/algoExpert/demo/Repository/Service/UserNotificationService.java
+++ backend/src/main/java/com/algoExpert/demo/Repository/Service/UserNotificationService.java
@@ -1,12 +1,14 @@
 package com.algoExpert.demo.Repository.Service;
 
 import com.algoExpert.demo.Entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserNotificationService {
 
-     void createNotification(User user, String notifMsg,int taskId);
+     void createNotification(User user, String notifMsg);
+     @Transactional
      void deleteNotification(int id);
      boolean isDuplicate(Integer userId,Integer taskId);
 
