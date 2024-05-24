abd31b0ef2afde83141d77b2ea37089778d8c007
santos <santoschrist1234@gmail.com>
2024-05-24 13:07:12 +0200

    update: added interface method for notification

--- backend/src/main/java/com/algoExpert/demo/Repository/Service/UserNotificationService.java
+++ backend/src/main/java/com/algoExpert/demo/Repository/Service/UserNotificationService.java
@@ -1,8 +1,11 @@
 package com.algoExpert.demo.Repository.Service;
 
 import com.algoExpert.demo.Entity.User;
+import com.algoExpert.demo.Entity.UserNotification;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface UserNotificationService {
@@ -11,5 +14,6 @@ public interface UserNotificationService {
      @Transactional
      void deleteNotification(int id);
      boolean isDuplicate(Integer userId,Integer taskId);
+     List<UserNotification> userNotifications();
 
 }
