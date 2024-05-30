31d2460b89c6cdfa23adc2f154a92b5854c4129a
santos <santoschrist1234@gmail.com>
2024-05-30 14:07:05 +0200

    cleanup

--- backend/src/main/java/com/algoExpert/demo/Mapper/TableMapper.java
+++ backend/src/main/java/com/algoExpert/demo/Mapper/TableMapper.java
@@ -6,19 +6,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TableMapper {
-    public static TableDto mapToTableDto(TaskContainer taskContainer) {
-        return new TableDto(
-                taskContainer.getTable_id(),
-                taskContainer.getTable_name(),
-                taskContainer.getTasks()
-        );
-    }
+//    public static TableDto mapToTableDto(TaskContainer taskContainer) {
+//        return new TableDto(
+//                taskContainer.getTableId(),
+//                taskContainer.getTableName(),
+//                taskContainer.getTasks()
+//        );
+//    }
 
-    public static TaskContainer mapToTaskTable(TableDto taskTableDto) {
-        return new TaskContainer(
-                taskTableDto.getTable_id(),
-                taskTableDto.getTable_name(),
-                taskTableDto.getTasks()
-        );
-    }
+//    public static TaskContainer mapToTaskTable(TableDto taskTableDto) {
+//        return new TaskContainer(
+//                taskTableDto.getTable_id(),
+//                taskTableDto.getTable_name(),
+//                taskTableDto.getProjectId(),
+//                taskTableDto.getTasks()
+//        );
+//    }
 }
