aa7b443eadb524f3b5b6c0898514b17bd9ef02ff
santos <santoschrist1234@gmail.com>
2024-05-30 14:04:54 +0200

    add:added pagination for table

--- backend/src/main/java/com/algoExpert/demo/Repository/Service/Impl/ProjectUserImpl.java
+++ backend/src/main/java/com/algoExpert/demo/Repository/Service/Impl/ProjectUserImpl.java
@@ -1,29 +1,26 @@
 package com.algoExpert.demo.Repository.Service.Impl;
 
-import com.algoExpert.demo.Entity.Member;
-import com.algoExpert.demo.Entity.Project;
-import com.algoExpert.demo.Entity.RefreshToken;
-import com.algoExpert.demo.Entity.User;
+import com.algoExpert.demo.Entity.*;
 import com.algoExpert.demo.ExceptionHandler.InvalidArgument;
 import com.algoExpert.demo.Jwt.JwtResponse;
 import com.algoExpert.demo.Jwt.JwtService;
-import com.algoExpert.demo.Repository.MemberRepository;
-import com.algoExpert.demo.Repository.ProjectRepository;
-import com.algoExpert.demo.Repository.RefreshTokenRepository;
+import com.algoExpert.demo.Repository.*;
 import com.algoExpert.demo.Repository.Service.ProjectUserService;
-import com.algoExpert.demo.Repository.UserRepository;
 import com.algoExpert.demo.role.Role;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static com.algoExpert.demo.AppUtils.AppConstants.*;
@@ -48,6 +45,9 @@ public class ProjectUserImpl implements ProjectUserService {
 
     @Autowired
     private RefreshTokenSevice refreshTokenSevice;
+
+    @Autowired
+    private TableRepository tableRepository;
 
 
     @Override
@@ -84,6 +84,44 @@ public class ProjectUserImpl implements ProjectUserService {
     }
 
     @Override
+    public ResponseEntity<Project> getProjectWithSortedAndSearchedTables(int projectId, int page, int size, String search, String sortField, String sortDirection) {
+        Project foundProject= projectRepository.findById(projectId).orElseThrow(() -> new InvalidArgument(String.format(PROJECT_NOT_FOUND,projectId)));
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User projectUser = null;
+
+        if (authentication != null) {
+            projectUser = (User) authentication.getPrincipal();
+        }
+
+        //clear the roles of the user who is loading the project
+        List<Role> roleList = Objects.requireNonNull(projectUser).getRoles();
+        roleList.clear();
+        log.info("clear roles:{} ",roleList);
+//        //assign the role of the user that all users should have
+        roleList.add(Role.valueOf(USER_ROLE));
+        log.info("add roles:{} ",roleList);
+        for (Member member : foundProject.getMemberList() ){
+            if(member.getProjectRole().equals(OWNER_ROLE) && member.getUser_id().equals(projectUser.getUser_id()) ){
+                //find if the member has a role of an onwer and assign owner role if you find
+                roleList.add(Role.valueOf(OWNER_ROLE));
+            }
+            else if(member.getUser_id().equals(projectUser.getUser_id()) && member.getProjectRole().equals(MEMBER_ROLE)){
+                //assign role of a member if you find the id matching the user id who is loading the project
+                roleList.add(Role.valueOf(MEMBER_ROLE));
+            }
+        }
+        projectUser.setRoles(roleList);
+
+        userRepository.save(projectUser);
+
+        getSortedAndSearchedTables(foundProject,page,size,search,sortField,sortDirection);
+        return ResponseEntity.ok(foundProject);
+    }
+
+
+
+    @Override
     public JwtResponse refreshJwtToken(String refreshTokenRequest){
         return  refreshTokenRepository.findByToken(refreshTokenRequest)
                 .map(RefreshToken::getUser)
@@ -101,6 +139,25 @@ public class ProjectUserImpl implements ProjectUserService {
     @Override
     public Page<User> getUsersWithPagination(int offset, int pageSize) {
         return userRepository.findAll(PageRequest.of(offset,pageSize));
+    }
+
+
+    public void getSortedAndSearchedTables(Project project, int page, int size, String search, String sortField, String sortDirection){
+
+        Sort.Direction direction = sortDirection.equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Sort sort = Sort.by(direction, sortField);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        List<TaskContainer> tables;
+
+        if (search != null) {
+            tables = tableRepository.findByProjectIdAndTableNameContainingIgnoreCase(project.getProjectId(),search,pageable).toList();
+
+        } else {
+            tables = tableRepository.findByProjectId(project.getProjectId(),pageable).toList();;
+        }
+
+        project.setTables(tables);
     }
 
 
