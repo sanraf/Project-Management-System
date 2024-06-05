b284f5df80b84f2418344c59137c42845d699772
santos <santoschrist1234@gmail.com>
2024-06-05 13:58:36 +0200

    create:findByProjectId jpa query

--- backend/src/main/java/com/algoExpert/demo/Repository/MemberRepository.java
+++ backend/src/main/java/com/algoExpert/demo/Repository/MemberRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     @Query("SELECT COUNT(DISTINCT user_id) FROM Member WHERE projectRole = 'OWNER'")
@@ -12,4 +14,5 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
 
     @Query("SELECT COUNT(DISTINCT user_id) FROM Member WHERE projectRole = 'MEMBER'")
     Integer totalProjectMembers();
+    List<Member> findByProjectId(Integer projectId);
 }
